ba684475d68b406e2644c81f59efc74d9cf2c0d2
Lokesh <lokesh.anddev@gmail.com>
2018-03-16 00:05:09 +0530

    flavour structure initialised

---
+++ app/src/customer/java/com/lokeshponnada/flavourdemo/HomePage.java
@@ -0,0 +1,23 @@
+package com.lokeshponnada.flavourdemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_home);
+
+        findViewById(R.id.tv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(HomePage.this,CommonActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
